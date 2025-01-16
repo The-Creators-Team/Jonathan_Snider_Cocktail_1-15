433bca9f20a522c7668fb1467090b746c67392df
JonathanSnider <Jonathan.Snider@techconsulting.tech>
2025-01-16 01:06:26 -0500

    Added placeholder and error images for image loading, thank the Lord

--- cocktail/src/main/java/com/example/cocktail/SearchDrinkByNameScreen.kt
+++ cocktail/src/main/java/com/example/cocktail/SearchDrinkByNameScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.example.common.model.Drink
@@ -132,6 +133,8 @@ fun NetworkImageDisplay(imageUrl: String) {
     AsyncImage(
         model = imageUrl,
         contentDescription = "Drink Image",
+        placeholder = painterResource(id = R.drawable.ic_gear), // Your placeholder resource
+        error = painterResource(id = R.drawable.ic_cancel),
         modifier = Modifier
             .size(50.dp)
             .clip(CircleShape)
